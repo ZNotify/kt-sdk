d902369f58da2337bc106445c69e511e0bd78e95
Zxilly <zxilly@outlook.com>
2022-06-09 22:21:50 +0800

    feat(WIP): prepare for release on Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,19 @@
 @file:Suppress("UnstableApiUsage")
 
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     kotlin("multiplatform") version "1.6.21"
     kotlin("plugin.serialization") version "1.6.21"
+    id("kotlinx-atomicfu") version "0.17.3"
     id("com.android.library")
+    id("maven-publish")
+    id("signing")
 }
 
-group = "dev.zxilly.notify"
+group = "dev.zxilly"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,8 +21,15 @@ repositories {
     mavenCentral()
 }
 
+ext["kotlin_version"] = "1.6.21"
 
 kotlin {
+    sourceSets.all {
+        languageSettings.apply {
+            languageVersion = "1.6"
+            apiVersion = "1.6"
+        }
+    }
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
@@ -34,9 +48,10 @@ kotlin {
     }
 
 
-    android("android"){
+    android("android") {
         publishLibraryVariants("release", "debug")
     }
+
     sourceSets {
         val ktorVersion = "2.0.2"
         val serializationVersion = "1.3.3"
@@ -44,7 +59,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
             }
         }
         val commonTest by getting {
@@ -58,9 +73,6 @@ kotlin {
                 implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
             }
         }
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
         val androidMain by getting {
             dependencies {
                 rootProject
@@ -87,4 +99,72 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+fun String.mapToEnv(): String {
+    return this.toUpperCase().replace(".", "_")
+}
+
+val props = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
+
+fun getKey(key: String): String {
+    return props.getProperty(key) ?: (System.getenv(key.mapToEnv())
+        ?: throw IllegalArgumentException("$key is not defined"))
+}
+
+val mavenUser = getKey("maven.user")
+val mavenPassword = getKey("maven.password")
+val signingKey = getKey("signing.key")
+val signingPassword = getKey("signing.password")
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = mavenUser
+                password = mavenPassword
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar.get())
+
+        pom {
+            name.set("ZNotify Kotlin SDK")
+            description.set("Kotlin multi platform sdk for ZNotify")
+            url.set("https://github.com/ZNotify/kt-sdk")
+
+            licenses {
+                license {
+                    name.set("Apache-2.0")
+                    url.set("https://opensource.org/licenses/Apache-2.0")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Zxilly")
+                    name.set("Zxilly")
+                    email.set("zxilly@outlook.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/ZNotify/kt-sdk.git")
+            }
+
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,8 +9,11 @@ pluginManagement {
             if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android") {
                 useModule("com.android.tools.build:gradle:4.2.2")
             }
+            if (requested.id.id == "kotlinx-atomicfu") {
+                useModule("org.jetbrains.kotlinx:atomicfu-gradle-plugin:${requested.version}")
+            }
         }
     }
 }
-rootProject.name = "notify-kt-sdk"
+rootProject.name = "notify-sdk"
 
