ccf73a85ac376e2cea23b31540294cd9d015b00b
Zxilly <zxilly@outlook.com>
2022-12-21 12:23:11 +0800

    build: publish gradle analysis

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,19 @@ pluginManagement {
         }
     }
 }
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "notify-sdk"
 
