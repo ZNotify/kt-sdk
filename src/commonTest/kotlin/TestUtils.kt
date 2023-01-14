c050e93f3c4254e2d115768e1e5260375dd76e53
Zxilly <zhouxinyu1001@gmail.com>
2023-01-14 17:28:42 +0800

    test: add useragent
    [skip ci]

--- src/commonTest/kotlin/TestUtils.kt
+++ src/commonTest/kotlin/TestUtils.kt
@@ -1,5 +1,6 @@
 import dev.zxilly.notify.sdk.defaultEndpoint
 import io.ktor.client.*
+import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import kotlin.test.fail
@@ -8,7 +9,11 @@ import kotlin.test.fail
 object TestUtils {
     suspend fun check() {
         try {
-            with(HttpClient().get("$defaultEndpoint/alive")) {
+            with(HttpClient(){
+                install(UserAgent) {
+                    agent = "Kotlin SDK test util"
+                }
+            }.get("$defaultEndpoint/alive")) {
                 if (status != HttpStatusCode.NoContent) {
                     fail("Test server is not running")
                 }
