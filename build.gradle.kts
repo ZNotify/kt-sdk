4140ef6f8943273c97e65ab6af2c8a623d2855c9
Zxilly <zhouxinyu1001@gmail.com>
2023-01-20 23:29:18 +0800

    build: use java 8 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ keeper {
 
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(8))
         vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 
