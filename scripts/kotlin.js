fe6ffb12cb0d2366d8ac6d0340ce5d453454caf1
Zxilly <zhouxinyu1001@gmail.com>
2023-01-21 15:35:01 +0800

    ci: enhance get kotlin version

---
+++ scripts/kotlin.js
@@ -0,0 +1,13 @@
+// Path: scripts\kotlin.js
+
+const fs = require('fs');
+const path = require('path');
+
+const file = fs.readFileSync(path.join(__dirname, '../build.gradle.kts'), 'utf8');
+const match = file.match(/val ktVersion = "(.*)"/);
+const version = match[1];
+
+console.log(version);
+
+// write to build/kotlinVersion.txt
+fs.writeFileSync(path.join(__dirname, '../build/kotlinVersion.txt'), version);
